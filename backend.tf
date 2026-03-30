0de7d1d864f1037a2dd4f9e1cc1698ec4db73172
chinmaya <chinmaya.dob1999@gmail.com>
2026-03-30 23:44:30 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "cm-bucket-terraform"
-        key    = "prod/terraform.tfstate"
-        encrypt = true
-        dynamodb_table = "demo-terraform-db"
-        region = "ap-south-1"
-    }
-}
